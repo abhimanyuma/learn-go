5402bf51e4f2c282ee42cb203e2fb4307d6bb9f9
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-13 05:49:33 +0530

    Add more to random

--- learn/random.go
+++ learn/random.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
-import "math/rand"
+import (
+  "fmt"
+  "math/rand"
+  "time"
+  // "bytes"
+  "math"
+)
 
 func main(){
+  rand.Seed( time.Now().UTC().UnixNano())
   fmt.Println("My favourite number is",rand.Intn(43));
+  fmt.Println("Now you have %g problems",math.Nextafter(2,3));
 }
