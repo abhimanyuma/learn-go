2d4c6d2d20d7ffbe46a11b18c4bc2b408b07a334
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-13 06:51:04 +0530

    Modify potpourri

--- learn/potpourri.go
+++ learn/potpourri.go
@@ -7,9 +7,16 @@ import (
   // "bytes"
   "math"
 )
+func sum(a int,b int) int {
+	return (a+b)
+}
+
 
 func main(){
   rand.Seed( time.Now().UTC().UnixNano())
+  fmt.Println("π = ",math.Pi," e = ",math.E)
+  fmt.Println("My constant = ",(math.Pi*math.Pi)/(math.E+math.E))
   fmt.Println("My favourite number is",rand.Intn(43));
-  fmt.Println("Now you have %g problems",math.Nextafter(2,3));
+  fmt.Printf("Now you have %d problems\n",
+  	          sum(2,3));
 }
