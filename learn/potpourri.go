097aef58a9d5c7a95c8aed767470da8bbf5206b8
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-13 11:24:23 +0530

    Add const

--- learn/potpourri.go
+++ learn/potpourri.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  // "bytes"
-  "math"
+  	"fmt"
+  	"math/rand"
+  	"time"
+  	// "bytes"
+  	"math"
+  	"math/cmplx"
 )
+var (
+	ToBe 	bool 		= false
+	MaxInt 	uint64 		= 1<<64 - 1
+	z      	complex128 	= cmplx.Sqrt(-5 + 12i)
+)
+
 //Var to define variables //Now adding initial values
 var alpha,beta,gamma int = 42,0,21
 
@@ -40,13 +47,19 @@ func main(){
 	//Omitting var by using
 	iscorrect,iswrong,isascii := true,false,"Yes!" 
 
-  	rand.Seed( time.Now().UTC().UnixNano())
-  	fmt.Println("π = ",math.Pi," e = ",math.E)
-  	fmt.Println("My constant = ",(math.Pi*math.Pi)/(math.E+math.E))
-  	fmt.Println("My favourite number is",rand.Intn(43));
-  	fmt.Printf("Now you have %d problems\n",
-  	          	sum(2,3));
-  	fmt.Println(swap("Halla","Bazinga"))
-  	fmt.Println(extendedGCD(2,3))
-  	fmt.Println(alpha,beta,gamma,iswrong,iscorrect,isascii)
+	const pi = math.Pi
+	const e  = math.E
+	rand.Seed( time.Now().UTC().UnixNano())
+	fmt.Println("π = ",pi," e = ",e)
+	fmt.Println("My constant = ",(math.Pi*math.Pi)/(math.E+math.E))
+	fmt.Println("My favourite number is",rand.Intn(43));
+	fmt.Printf("Now you have %d problems\n",
+				sum(2,3));
+	fmt.Println(swap("Halla","Bazinga"))
+	fmt.Println(extendedGCD(2,3))
+	fmt.Println(alpha,beta,gamma,iswrong,iscorrect,isascii)
+	const f = "%T(%v)\n"
+    fmt.Printf(f, ToBe, ToBe)
+    fmt.Printf(f, MaxInt, MaxInt)
+    fmt.Printf(f, z, z)
 }
